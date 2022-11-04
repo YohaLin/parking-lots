1877312f8c2172bb518fbb8324d1f547a71993bf
YohaLin <kate1995y@gmail.com>
2022-11-04 12:40:32 +0800

    feature: marker, redux, position, parking-lots api

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,10 +1,18 @@
+import { useDispatch } from 'react-redux';
 import Icons from "../assets/images/icons"
 
-function Information() {
+function Information({data}) {
+  const dispatch = useDispatch()
+
+  // 不同情況dispatch不同type
+  const myPositionHandler = () => {
+    dispatch({ type: 'myPosition'})
+  }
+
   return (
     <div className="info_card">
       <div className="info__position">
-        <Icons.SVGPosition />
+        <Icons.SVGPosition onClick={myPositionHandler} />
       </div>
       <div className="info__container">
         <div className="info__dragger">
@@ -12,9 +20,9 @@ function Information() {
         <div className="info__above">
           <div className="info__above-content">
             <div className="info__above-left">
-              <h3>府前廣場地下停車場</h3>
-              <p>總車位 1998 空位數 0</p>
-              <p>30元/小時</p>
+              <h3>{data.name}</h3>
+              <p>總車位 {data.totalcar} 空位數 0</p>
+              <p>{data.FareInfo}元/小時</p>
             </div>
             <div className="info__above-right">
               <Icons.SVGNavigator />
@@ -25,10 +33,10 @@ function Information() {
         <hr/>
         <div className="info__below">
           <div className="info__below-detail">
-            <p>營業時間： 00:00:00~23:59:59</p>
-            <p>費率： 小型車全日月票4200元，周邊里里民全日月票3360元，所在里里民全日月票2940元，夜間月票1000元(限周一至周五19-8，及周六、日與行政機關放假之紀念日、民俗日)，小型車計時30元(9-18)，夜間計時10元(18-9)；機車計時10元(當日當次上限20元)，機車月票300元。</p>
-            <p>地址： 台北市信義區松壽路1號地下 </p>
-            <p>聯絡電話： 02-27057716</p>
+            <p>營業時間： {data.serviceTime}</p>
+            <p>費率： {data.payex}</p>
+            <p>地址： {data.area + data.address} </p>
+            <p>聯絡電話： 02-{data.tel}</p>
           </div>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Provider } from 'react-redux'
+import store from './store/store'
 import "./assets/styles/main.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  //開發模式下會出現console.log很多次，部署後就不會一直印了
+// 這樣在App.js裡面的所有component都可以用到redux
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
